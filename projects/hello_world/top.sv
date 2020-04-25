5149225d08fb3fe287d481736821d09e2647a66b
Istvan Csomortani <istvan.csomortani@analog.com>
2020-04-25 15:21:04 +0300

    hello_world: Connect clock to DUT

--- projects/hello_world/top.sv
+++ projects/hello_world/top.sv
@@ -4,12 +4,21 @@ module top;
   import uvm_pkg::*;
   import my_pkg::*;
 
+  //clock generation
+
+  bit clk;
+
+  always #5 clk=~clk;
+
   // interface instance
-  dut_if dut_if1 ();
+
+  dut_if dut_if1 (clk);
 
   // DUT instance
+
   dut i_dut ( ._if(dut_if1) );
 
+  // run test
   initial 
   begin
 	  run_test("my_first_test");
